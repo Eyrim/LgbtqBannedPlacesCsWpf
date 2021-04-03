704e0e4ab493adf5cb029c46e1a463b8e76d68ed
Eyrim <marfxedbot@gmail.com>
2021-04-03 16:17:44 +0100

    Second commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,24 +27,32 @@ namespace LgbtqBannedPlacesCsWpf
             InitializeComponent();
         }
 
+        /// <summary>
+        /// When the button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnSubmit_Click(object sender, RoutedEventArgs e)
         {
+            // Creates the dictionary for the user's identity
             Dictionary<string, string> identity =
                 new Dictionary<string, string>(); // identity.Add();
 
+            // Creates objects for each combobox, when the user adds their identity
             ComboBoxItem cbiAgab = (ComboBoxItem)comboAgab.SelectedItem;
             ComboBoxItem cbiGender = (ComboBoxItem)comboGender.SelectedItem;
             ComboBoxItem cbiRomantic = (ComboBoxItem)comboRomantic.SelectedItem;
             ComboBoxItem cbiSexual = (ComboBoxItem)comboSexual.SelectedItem;
             ComboBoxItem cbiPresentation = (ComboBoxItem)comboPresentation.SelectedItem;
 
+            // Turns all of those objects into strings
             identity.Add("agab", cbiAgab.Content.ToString()); //TODO: FIx blank input bug causing crash
             identity.Add("gender", cbiGender.Content.ToString());
             identity.Add("romantic", cbiRomantic.Content.ToString());
             identity.Add("sexual", cbiSexual.Content.ToString());
             identity.Add("presentation", cbiPresentation.Content.ToString());
 
-
+            // Spits out the identity to the user (debug)
             MessageBox.Show($@"
             You are: {cbiAgab.Content.ToString()}
             Your gender identity: {cbiGender.Content.ToString()}
@@ -53,29 +61,45 @@ namespace LgbtqBannedPlacesCsWpf
             You present: {cbiPresentation.Content.ToString()}
             ");
 
+            // Creates a dictionary for the JSON to sit in
             Dictionary<string, string> hate = new Dictionary<string, string>();
 
-            StreamReader sr = new StreamReader("C:\\Users\\marfx\\source\\repos\\LgbtqBannedPlacesCsWpf\\hate.json");
+            // Creates a streamreader object for reading hateCountries.json
+            StreamReader sr = new StreamReader("C:\\Projects\\LbtqBannedPlacesCsWpf2\\LgbtqBannedPlacesCsWpf\\hateCountries.json");
 
+            // Reads hateCountries.json and puts it in json (the string, not the format)
             string json = sr.ReadToEnd();
 
+            // Creates a list for all the countries with anti-lgbtq+ laws
+            List<string> countries = new List<string>();
+
+            // Reads the JSON values
             JsonTextReader reader = new JsonTextReader(new StringReader(json));
+            // Operates as a counter (I hate it too, don't worry)
+            int i = 0;
+            // While there is more to be read
             while (reader.Read())
-            {
+            { 
                 switch (reader.Value)
                 {
+                    // If the value is null then stop
                     case null:
                         break;
 
+                    // Else, if it isn't startobject (Not useful to me rn) then add it to the list
                     default:
                         if (Convert.ToString(reader.TokenType) != "StartObject")
                         {
-                            hate.Add()
+                            //MessageBox.Show($"{reader.TokenType}, {reader.Value}"); // Country, empty string
+                            countries.Add(reader.Value.ToString()); // Adding to the list
+                            
+                            // Increment the counter
+                            ++i;
                         }
 
                         break;
                 }
-                /*
+                /* (FROM DOCS)
                 if (reader.Value != null)
                 {
                     MessageBox.Show($"Token: {reader.TokenType}, Value: {reader.Value}");
@@ -86,6 +110,90 @@ namespace LgbtqBannedPlacesCsWpf
                 }
                 */
             }
+            
+            // An array to store the countries without the blank strings
+            string[] countriesAmmended = new string[countries.Count];
+            int counter = 0;
+
+            // For every element in countries
+            for (int j = 0; j < countries.Count; ++j)
+            {
+                // If it isn't whitespace
+                if (countries[j] != "")
+                { 
+                    // Add it to the array
+                    countriesAmmended.SetValue(countries[j], ++counter);
+                }
+            }
+
+            bool isCis = true;
+            bool presentsCis = true;
+            bool isHet = true;
+
+            // If the user isn't cis
+            if (identity["agab"] != identity["gender"]) { isCis = false; }
+
+            // If the user is assigned GNC
+            if (identity["agab"] != identity["presentation"]) { presentsCis = false; }
+
+            // If the user isn't heterosexual
+            if (identity["sexual"] != "Heterosexual") { isHet = false; }
+
+            // If the user isn't heteromantic
+            if (identity["romantic"] != "Heteromantic") { isHet = false; }
+
+
+            User user = new User(isCis, isHet, presentsCis);
+
+            displayCountries(user, countriesAmmended);
+        }
+
+        /// <summary>
+        /// A method which calls the transphobic and homophobic countries to be called
+        /// </summary>
+        /// <param name="user"></param>
+        void displayCountries(User user, string[] countriesAmmended)
+        {
+            if (user.IsCis == false || user.PresentsCis == false)
+            {
+                displayTransphobic(countriesAmmended);
+            }
+
+            if (user.IsHet == false)
+            {
+                displayHomophobic();
+            }
+        }
+
+        static string deleteSubstring(string content, string substring)
+        {
+            int index = content.IndexOf(substring);
+            string output = "";
+
+            for (int i = 0; i < content.Length; i++) 
+            { 
+                if (i == index) { return output; }
+                output += content[i]; 
+            }
+
+            return output;
+        }
+
+        void displayTransphobic(string[] countriesAmmended)
+        {
+            string country = "";
+            string toDisplay = "";
+            for (int i = 0; i < countriesAmmended.Length; i++)
+            {
+                if (Convert.ToBoolean(countriesAmmended[i]?.Contains("(gender)"))) { toDisplay += deleteSubstring(countriesAmmended[i], "(") + "\n"; };
+            }
+
+            txtbCountries.Text = toDisplay;
+        }
+
+        static void displayHomophobic()
+        {
+
         }
     }
 }
---
+++ User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LgbtqBannedPlacesCsWpf
+{
+    public class User
+    {
+        public bool IsCis;
+        public bool IsHet;
+        public bool PresentsCis;
+
+        /// <summary>
+        /// Implementation of user
+        /// </summary>
+        /// <param name="cis">bool; true if the user is cisgender</param>
+        /// <param name="het">bool; true if the user is heterosexual AND heteromantic</param>
+        /// <param name="pCis">bool; true if the user isn't GNC</param>
+        public User(bool cis, bool het, bool pCis)
+        {
+            IsCis = cis;
+            IsHet = het;
+            PresentsCis = pCis;
+        }
+    }
+}
